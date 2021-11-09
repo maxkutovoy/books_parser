0c033f537d35f246e857c41aa8ea6c0c59afa60e
Max Kutovoy <maxkutovoy@yandex.ru>
2021-11-09 18:35:55 +0600

    Добавил функцию скачиания жанров

--- main.py
+++ main.py
@@ -24,51 +24,39 @@ def download_book(image_dir, book_id):
         book.write(response.text)
 
 
-def download(url, filename, folder='Books/'):
-    """Функция для скачивания текстовых файлов.
-    Args:
-        url (str): Cсылка на текст, который хочется скачать.
-        filename (str): Имя файла, с которым сохранять.
-        folder (str): Папка, куда сохранять.
-    Returns:
-        str: Путь до файла, куда сохранён текст.
-    """
-
-
-def get_book_info(book_id):
-    url = f'https://tululu.org/b{book_id}'
-    response = requests.get(url)
-    response.raise_for_status()
-
-    soup = BeautifulSoup(response.text, 'lxml')
-    title_tag = soup.find('h1')
+def get_book_info(book_soup):
+    title_tag = book_soup.find('h1')
     title_text = title_tag.text
-    try:
-        book_title, author = title_text.split('   ::   ')
-    except ValueError:
-        return None, None
-    img = soup.find(class_="bookimage").find('img')['src']
+    book_title, author = title_text.split('   ::   ')
+    return book_title, author
+
+
+def get_image_url(book_soup):
+    img = book_soup.find(class_="bookimage").find('img')['src']
     img_url = (urljoin('https://tululu.org', img))
-    try:
-        comments = soup.find_all('div', class_="texts")
-        for comment in comments:
-            c = comment.find('span', class_='black')
-            print(f'Комментарий: {c.text}')
-    except:
-        pass
-    return book_title, img_url
+    return img_url
 
 
-def download_txt(id, book_title, folder='books/'):
-    url = f'https://tululu.org/txt.php?id={id}'
+def get_comments(book_soup):
+    comments_tag = book_soup.find_all('div', class_="texts")
+    comments = [comment.find('span', class_='black').text for comment in comments_tag]
+    return comments
+
+
+def get_genre(book_soup):
+    genres_tag = book_soup.find('span', class_="d_book").find_all('a')
+    genres = [genre.text for genre in genres_tag]
+    return genres
+
+
+def download_txt(url, book_title, folder='books/'):
     response = requests.get(url)
     response.raise_for_status()
-    check_for_redirect(response)
     print(book_title)
     filename = f'{id}. {sanitize_filename(book_title)}'
     filepath = os.path.join(folder, filename)
-    with open(f'{filepath}.txt', 'w') as book:
-        book.write(response.text)
+    # with open(f'{filepath}.txt', 'w') as book:
+    #     book.write(response.text)
 
 
 def download_image(url, folder='images/'):
@@ -77,8 +65,8 @@ def download_image(url, folder='images/'):
     parsed_url = urlsplit(url)
     filename = os.path.split(unquote(parsed_url.path))[1]
     filepath = os.path.join(folder, filename)
-    with open(f'{filepath}', 'wb') as book:
-        book.write(response.content)
+    # with open(f'{filepath}', 'wb') as book:
+    #     book.write(response.content)
 
 
 if __name__ == '__main__':
@@ -88,10 +76,24 @@ if __name__ == '__main__':
     Path(images_dir).mkdir(parents=True, exist_ok=True)
     for book_id in range(1, 11):
         try:
-            book_title, img_url = get_book_info(book_id)
-            if book_title is not None:
-                download_txt(book_id, book_title, books_dir)
-                download_image(img_url, folder='images/')
+            book_url = f'https://tululu.org/b{book_id}'
+            download_url = f'https://tululu.org/txt.php?id={book_id}'
+
+            response = requests.get(download_url)
+            response.raise_for_status()
+            check_for_redirect(response)
+
+            response = requests.get(book_url)
+            response.raise_for_status()
+            book_soup = BeautifulSoup(response.text, 'lxml')
+
+            book_title, author = get_book_info(book_soup)
+            img_url = get_image_url(book_soup)
+            # download_txt(download_url, book_title, books_dir)
+            # download_image(img_url, folder='images/')
+            get_comments(book_soup)
+            # get_genre(book_soup)
+
         except requests.HTTPError:
             pass
 
