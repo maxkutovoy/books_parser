b156111473fc6f4b534b59669819c7e8d1f536d7
Max Kutovoy <maxkutovoy@yandex.ru>
2021-11-09 19:12:47 +0600

    Структурировал функцию получения информации о книге

--- main.py
+++ main.py
@@ -13,60 +13,43 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_book(image_dir, book_id):
-    url = f'https://tululu.org/txt.php?id={book_id}'
-
-    response = requests.get(url)
-    response.raise_for_status()
-    check_for_redirect(response)
-
-    with open(f'{image_dir}/book{book_id}.txt', 'w') as book:
-        book.write(response.text)
-
-
 def get_book_info(book_soup):
     title_tag = book_soup.find('h1')
     title_text = title_tag.text
     book_title, author = title_text.split('   ::   ')
-    return book_title, author
 
+    img_path = book_soup.find(class_="bookimage").find('img')['src']
+    img_url = (urljoin('https://tululu.org', img_path))
 
-def get_image_url(book_soup):
-    img = book_soup.find(class_="bookimage").find('img')['src']
-    img_url = (urljoin('https://tululu.org', img))
-    return img_url
-
-
-def get_comments(book_soup):
     comments_tag = book_soup.find_all('div', class_="texts")
     comments = [comment.find('span', class_='black').text for comment in comments_tag]
-    return comments
 
-
-def get_genre(book_soup):
     genres_tag = book_soup.find('span', class_="d_book").find_all('a')
     genres = [genre.text for genre in genres_tag]
-    return genres
+
+    book_info = {
+        'book_title': book_title,
+        'author': author,
+        'img_url': img_url,
+        'comments': comments,
+        'genres': genres,
+    }
+    return book_info
 
 
-def download_txt(url, book_title, folder='books/'):
-    response = requests.get(url)
-    response.raise_for_status()
-    print(book_title)
-    filename = f'{id}. {sanitize_filename(book_title)}'
+def download_txt(response, book_title, book_id, folder='books/'):
+    filename = f'{book_id}. {sanitize_filename(book_title)}'
     filepath = os.path.join(folder, filename)
-    # with open(f'{filepath}.txt', 'w') as book:
-    #     book.write(response.text)
+    with open(f'{filepath}.txt', 'w') as book:
+        book.write(response.text)
 
 
-def download_image(url, folder='images/'):
-    response = requests.get(url)
-    response.raise_for_status()
-    parsed_url = urlsplit(url)
+def download_image(response, img_url, folder='images/'):
+    parsed_url = urlsplit(img_url)
     filename = os.path.split(unquote(parsed_url.path))[1]
     filepath = os.path.join(folder, filename)
-    # with open(f'{filepath}', 'wb') as book:
-    #     book.write(response.content)
+    with open(f'{filepath}', 'wb') as book:
+        book.write(response.content)
 
 
 if __name__ == '__main__':
@@ -79,20 +62,17 @@ if __name__ == '__main__':
             book_url = f'https://tululu.org/b{book_id}'
             download_url = f'https://tululu.org/txt.php?id={book_id}'
 
-            response = requests.get(download_url)
-            response.raise_for_status()
-            check_for_redirect(response)
+            download_page = requests.get(download_url)
+            download_page.raise_for_status()
+            check_for_redirect(download_page)
 
-            response = requests.get(book_url)
-            response.raise_for_status()
-            book_soup = BeautifulSoup(response.text, 'lxml')
+            book_page = requests.get(book_url)
+            book_page.raise_for_status()
+            book_page = BeautifulSoup(book_page.text, 'lxml')
 
-            book_title, author = get_book_info(book_soup)
-            img_url = get_image_url(book_soup)
-            # download_txt(download_url, book_title, books_dir)
-            # download_image(img_url, folder='images/')
-            get_comments(book_soup)
-            # get_genre(book_soup)
+            book_info = get_book_info(book_page)
+            download_txt(download_page, book_info['book_title'], book_id, books_dir)
+            download_image(download_page, book_info['img_url'], images_dir)
 
         except requests.HTTPError:
             pass
