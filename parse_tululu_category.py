e73bcf5d89771c60cc08301a0c1ebb59b427eff3
Max Kutovoy <maxkutovoy@yandex.ru>
2022-01-16 10:10:57 +0600

    Добавил получение ссылок на книги с нескольких страниц

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -9,16 +9,21 @@ import requests
 
 
 def get_books():
-    base_url =  'https://tululu.org'
-    sifi_books_url = 'https://tululu.org/l55/'
-    sifi_books_response = requests.get(sifi_books_url)
+    base_url = 'https://tululu.org'
 
-    sifi_books_soup = BeautifulSoup(sifi_books_response.text, 'lxml')
-    books_on_page = sifi_books_soup.find_all('table', class_='d_book')
-    for book in books_on_page:
-        book_href = book.find('a')['href']
-        book_url = urljoin(base_url, book_href)
-        print(book_url)
+    pages_count = 10
+    for page_number in range(1, pages_count+1):
+        print(f'Page: {page_number}')
+        print('Books:')
+        sifi_books_url = f'https://tululu.org/l55/{page_number}'
+        sifi_books_response = requests.get(sifi_books_url)
+
+        sifi_books_soup = BeautifulSoup(sifi_books_response.text, 'lxml')
+        books_on_page = sifi_books_soup.find_all('table', class_='d_book')
+        for book in books_on_page:
+            book_href = book.find('a')['href']
+            book_url = urljoin(base_url, book_href)
+            print(book_url)
 
 
 get_books()
