ede2ddd3793e7cb99f681194e713dc985fbd1c62
Max Kutovoy <maxkutovoy@yandex.ru>
2022-02-05 18:04:46 +0600

    Разделил книги на 2 колонки

--- render_website.py
+++ render_website.py
@@ -1,6 +1,8 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from livereload import Server, shell
+from more_itertools import chunked
+from pprint import pprint
 
 
 def render_site():
@@ -14,7 +16,9 @@ def render_site():
     with open("books_description.json", "r") as my_file:
         books = json.load(my_file)
 
-    rendered_page = template.render(books=books)
+    splited_books = list(chunked(books, 2))
+
+    rendered_page = template.render(books=splited_books)
 
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
